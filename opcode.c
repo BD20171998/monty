2dc76e2ecaaa480047fa4bb8eda8591ba0a46c2b
Bob Deprizio <robert.deprizio@gmail.com>
2020-01-04 00:43:05 +0000

    Changed error handling to use dprintf and added an aedge case for pint

--- opcode.c
+++ opcode.c
@@ -7,17 +7,17 @@
  */
 void pall(stack_t **stack, unsigned int line_num)
 {
-        stack_t *full_stack;
+	stack_t *full_stack;
 	(void)line_num;
 
-        if (*stack == NULL)
-                return;
-        full_stack = *stack;
-        while (full_stack != NULL)
-        {
-                printf("%d\n", full_stack->n);
-                full_stack = full_stack->next;
-        }
+	if (*stack == NULL)
+		return;
+	full_stack = *stack;
+	while (full_stack != NULL)
+	{
+		printf("%d\n", full_stack->n);
+		full_stack = full_stack->next;
+	}
 }
 
 /**
@@ -27,35 +27,37 @@ void pall(stack_t **stack, unsigned int line_num)
  */
 void pint(stack_t **stack, unsigned int line_num)
 {
-        if (stack == NULL)
-        {
-                printf("L%u: can't pint, stack empty\n", line_num);
-                exit(EXIT_FAILURE);
-        }
-        else
-                printf("%d\n", (*stack)->n);
-
+	if (stack == NULL || *stack == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't pint, stack empty\n",
+			line_num);
+		exit(EXIT_FAILURE);
+	}
+	else
+		printf("%d\n", (*stack)->n);
 }
 
 /**
- * pint - remove element from stack
+ * pop - remove element from stack
  * @stack: stack of elements
  * @line_num: line number
  */
 void pop(stack_t **stack, unsigned int line_num)
 {
-        stack_t *next = NULL;
+	stack_t *next = NULL;
 
-        if (stack == NULL || *stack == NULL)
-        {
-                printf("L%u: can't pop an empty stack\n", line_num);
-                exit(EXIT_FAILURE);
-        }
+	if (stack == NULL || *stack == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't pop an empty stack\n",
+			line_num);
+		exit(EXIT_FAILURE);
+	}
 
-        next = (*stack)->next;
-        free(*stack);
-        *stack = next;
-        if (*stack == NULL)
-                return;
-        (*stack)->prev = NULL;
+	next = (*stack)->next;
+	free(*stack);
+	*stack = next;
+
+	if (*stack == NULL)
+		return;
+	(*stack)->prev = NULL;
 }
